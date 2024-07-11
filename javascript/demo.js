ee0e984799806e838758de57a6026acdc91da304
Sanskriti801 <154349220+Sanskriti801@users.noreply.github.com>
2024-07-11 07:04:38 +0530

    Delete javascript directory

--- javascript/demo.js
+++
@@ -1,7 +0,0 @@
-let helloBtn= document.querySelector('button');
-helloBtn.addEventListener('click', inputMsg);
-
-function inputMsg() {
-    let name = prompt('Enter Name of Student');
-    namasteBtn.textContent = 'Roll No. 1:' + name;
-}
